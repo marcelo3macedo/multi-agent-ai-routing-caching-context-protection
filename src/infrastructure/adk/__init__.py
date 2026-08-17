e7eac3f2e4463066ca219a019e7b24099a7df01c
Marcelo Macedo <marcelo3macedo@gmail.com>
2026-08-17 13:56:28 -0300

    feat: Sub-Agente de Filmes com ADK Tool (TMDB API)

--- src/infrastructure/adk/__init__.py
+++ src/infrastructure/adk/__init__.py
@@ -1,5 +1,7 @@
 from src.infrastructure.adk.agent import create_base_root_agent, create_root_agent, MONOLITHIC_SYSTEM_INSTRUCTION
 from src.infrastructure.adk.institutional_agent import create_institutional_agent, get_company_info
+from src.infrastructure.adk.movie_agent import create_movie_catalog_agent
+from src.infrastructure.adk.tmdb_tool import search_movies, truncate_movie_payload, tmdb_movie_tool
 from src.infrastructure.adk.runner import MonolithicAgentRunner
 from src.infrastructure.adk.mock_llm import register_mock_llm
 
@@ -8,6 +10,10 @@ __all__ = [
     "create_root_agent",
     "create_institutional_agent",
     "get_company_info",
+    "create_movie_catalog_agent",
+    "search_movies",
+    "truncate_movie_payload",
+    "tmdb_movie_tool",
     "MONOLITHIC_SYSTEM_INSTRUCTION",
     "MonolithicAgentRunner",
     "register_mock_llm"
